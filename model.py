509647838f8c7d50219822a271e864a8d778bf0b
SholaOg <93973141+SholaOg@users.noreply.github.com>
2022-03-04 14:41:15 +0100

    Update model.py

--- model.py
+++ model.py
@@ -58,7 +58,86 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    #predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    
+    # create new features
+    #Converting the object in the "time" to datetime 
+    feature_vector_df['time'] = pd.to_datetime(feature_vector_df['time'], format='%Y-%m-%d %H:%M:%S')
+
+    #Creting new features and re-ordering the df
+    feature_vector_df['year'] = pd.DatetimeIndex(feature_vector_df['time']).year
+    feature_vector_df['month'] = pd.DatetimeIndex(feature_vector_df['time']).month
+    feature_vector_df['day'] = pd.DatetimeIndex(feature_vector_df['time']).day
+    feature_vector_df['hour'] = pd.DatetimeIndex(feature_vector_df['time']).hour
+    
+    #Reordering df columns
+    column_titles = ['time', 'year', 'month', 'day', 'hour', 'Madrid_wind_speed', 
+                 'Valencia_wind_deg', 'Bilbao_rain_1h', 'Valencia_wind_speed', 'Seville_humidity', 
+                 'Madrid_humidity', 'Bilbao_clouds_all', 'Bilbao_wind_speed', 'Seville_clouds_all', 
+                 'Bilbao_wind_deg', 'Barcelona_wind_speed', 'Barcelona_wind_deg', 'Madrid_clouds_all', 
+                 'Seville_wind_speed', 'Barcelona_rain_1h', 'Seville_pressure', 'Seville_rain_1h', 
+                 'Bilbao_snow_3h', 'Barcelona_pressure', 'Seville_rain_3h', 'Madrid_rain_1h', 'Barcelona_rain_3h', 
+                 'Valencia_snow_3h', 'Madrid_weather_id', 'Barcelona_weather_id', 'Bilbao_pressure', 
+                 'Seville_weather_id', 'Valencia_pressure', 'Seville_temp_max', 'Madrid_pressure', 'Valencia_temp_max', 
+                 'Valencia_temp', 'Bilbao_weather_id', 'Seville_temp', 'Valencia_humidity', 'Valencia_temp_min', 
+                 'Barcelona_temp_max', 'Madrid_temp_max', 'Barcelona_temp', 'Bilbao_temp_min', 'Bilbao_temp', 
+                 'Barcelona_temp_min', 'Bilbao_temp_max', 'Seville_temp_min', 'Madrid_temp', 'Madrid_temp_min', 
+                 'load_shortfall_3h']
+    df=feature_vector_df.reindex(columns=column_titles)
+    
+    #Fill Valencia_pressure with the mean of each time of the day and day of the month.
+    for label, row in df.iterrows():
+        if pd.isnull(row['Valencia_pressure']):
+            y = row['year']
+            m = row['month']
+            d = row['day']
+            h = row['hour']
+
+            df.loc[label, 'Valencia_pressure'] = round((df[(df['year'] == y) & (df['month'] == m)]['Valencia_pressure']).mean(),1) 
+    
+    #df_dummy =df_dummy.reindex(columns=column_titles)
+    df_dummies = pd.get_dummies(df)
+    df_test_dummies = pd.get_dummies(df_test)
+
+    # Again we make sure that all the column names have underscores instead of whitespaces
+    df_dummies.columns = [col.replace(" ","_") for col in df_dummies.columns] 
+    df_test_dummies.columns = [col.replace(" ","_") for col in df_test_dummies.columns]
+    
+    #Reindexing the new DF such the load_shortfall_3h becomes the last column.
+    column_titles = [col for col in df_dummies.columns if col!= 'load_shortfall_3h'] + ['load_shortfall_3h']
+    df_dummies=df_dummies.reindex(columns=column_titles)
+    
+    from scipy.stats import pearsonr
+    # Build a dictionary of correlation coefficients and p-values
+    dict_cp = {}
+
+    column_titles = [col for col in corrs.index if col!= 'load_shortfall_3h']
+    for col in column_titles:
+        p_val = round(pearsonr(df_dummies[col], df_dummies['load_shortfall_3h'])[1],6)
+        dict_cp[col] = {'Correlation_Coefficient':corrs[col],
+                        'P_Value':p_val}
+
+    df_cp = pd.DataFrame(dict_cp).T
+    df_cp_sorted = df_cp.sort_values('P_Value')
+    df_cp_sorted[df_cp_sorted['P_Value']<0.1]
+    
+    #Building model with feature having p valve < 0.05
+    # The dependent variable remains the same:
+    y_data = df_dummies['load_shortfall_3h']  # y_name = 'load_shortfall_3h'
+
+    # Model building - Independent Variable (IV) DataFrame
+    X_names = list(df_cp[df_cp['P_Value'] < 0.05].index)
+    X_data = df_dummies[X_names]
+    
+    X_remove = ['Valencia_temp_min', 'Valencia_temp_max',
+            'Seville_temp_min', 'Seville_temp_max',
+            'Bilbao_temp_min', 'Bilbao_temp_max',
+            'Barcelona_temp_min', 'Barcelona_temp_max',
+            'Madrid_temp_min', 'Madrid_temp_max', 
+            'Seville_humidity', 'Madrid_humidity',
+            'Valencia_snow_3h', 'Sevill_rain_3h']
+             
+    predict_vector = [col for col in X_names if col not in X_remove]
     # ------------------------------------------------------------------------
 
     return predict_vector
